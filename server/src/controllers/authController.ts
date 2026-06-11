e7e90076e108c6833dd30e29adb7a6651081b19a
niranjanaa2006 <arulniranjanaa19@gmail.com>
2026-06-11 11:44:37 +0530

    feat: add user authentication with jwt login

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
-import User from "../models/user"
+import User from "../models/user";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
+// Register User
 export const registerUser = async (
   req: Request,
   res: Response
@@ -30,6 +31,51 @@ export const registerUser = async (
     res.status(201).json({
       message: "User registered successfully",
       user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+// Login User
+export const loginUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(400).json({
+        message: "Invalid credentials",
+      });
+      return;
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      res.status(400).json({
+        message: "Invalid credentials",
+      });
+      return;
+    }
+
+    const token = jwt.sign(
+      { id: user._id },
+      process.env.JWT_SECRET as string,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
     });
   } catch (error) {
     res.status(500).json({
--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { registerUser } from "../controllers/authController";
+import {
+  registerUser,
+  loginUser,
+} from "../controllers/authController";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
